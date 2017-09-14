2666cf1a1212b16bc87660eea56b0ec169c641be
xfarret <xfarret@gmail.com>
2017-09-14 09:27:47 +0200

    email

--- src/create_accounts.py
+++ src/create_accounts.py
@@ -7,8 +7,8 @@ gmailAccount = Account()
 gmailAccount.name = 'Gmail'
 gmailAccount.firstname = 'Xavier'
 gmailAccount.lastname = 'Farret'
-gmailAccount.email = 'xavier.farret@gmail.com'
-gmailAccount.password = 'doy8ksh6'
+gmailAccount.email = 'email@gmail.com'
+gmailAccount.password = 'password'
 gmailAccount.imap_server = 'imap.gmail.com'
 accountManager.add_account(gmailAccount)
 
@@ -16,8 +16,8 @@ freeAccount = Account()
 freeAccount.name = 'Free'
 freeAccount.firstname = 'Xavier'
 freeAccount.lastname = 'Farret'
-freeAccount.email = 'xfarret@free.fr'
-freeAccount.password = 'doy8ksh6'
+freeAccount.email = 'email@free.fr'
+freeAccount.password = 'password'
 freeAccount.imap_server = 'imap.free.fr'
 accountManager.add_account(freeAccount)
 
